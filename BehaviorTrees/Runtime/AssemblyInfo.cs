3a91e655d0b167551f5c95fae34f843563f38310
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-09-19 15:29:24 -0300

    Changed package structure

---
+++ BehaviorTrees/Runtime/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("HIAAC.BehaviorTree.Editor")]
+#endif
+[assembly: InternalsVisibleTo("HIAAC.BehaviorTree.Runtime")]
